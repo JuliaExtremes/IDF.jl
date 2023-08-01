17ef8dedd6491ebf45373683b2350705e6666be8
Auguste Paoli <auguste.paoli@polytechnique.edu>
2023-08-01 09:51:22 -0400

    tests added for fittedMLE

--- test/fittedmodels/test_fittedBayesian.jl
+++ test/fittedmodels/test_fittedBayesian.jl
@@ -1,5 +1,20 @@
 @testset "fitteBayesian.jl" begin 
 
+    D_values = [5,60,360]
+    model = IDF.SimpleScalingModel(D_values)
+    params = [3,2,0.1,0.7]
+    θ̂ = IDF.transformParams(model, params)
+
+    n_chain = 100
+    chain_array = Array{Float64, 3}(undef, n_chain, 4, 1)
+    for i in 1:n_chain
+        for j in 1:4
+            chain_array[i,j,1] = rand(Normal(θ̂[j], sqrt(j)))
+        end
+    end
+
+    chain = IDF.MambaLite.Chains(chain_array)
+    println(chain)
     @testset "getChainParam()" begin
     end
 
--- test/fittedmodels/test_fittedMLE.jl
+++ test/fittedmodels/test_fittedMLE.jl
@@ -1,12 +1,45 @@
 @testset "fittedMLE.jl" begin
 
+    D_values = [5,60,360]
+    model = IDF.SimpleScalingModel(D_values)
+    params = [3,2,0.1,0.7]
+    θ̂ = IDF.transformParams(model, params)
+    I_Fisher = Matrix{Float64}(I, 4, 4)
+
+    fitted_mle = IDF.FittedMLE(model, θ̂, I_Fisher)
+
     @testset "cint()" begin
+
+        g(θ) = exp(θ[1])
+        Δg(θ) = [exp(θ[1]), 0, 0, 0]
+        G = Δg(fitted_mle.θ̂)
+        var = G'*G
+
+        distrib = IDF.Normal(g(θ̂), sqrt(var))
+
+        cint = IDF.cint(fitted_mle, g)
+        @test cdf(distrib, cint[2]) - cdf(distrib, cint[1]) ≈ 0.95
+
+        cint = IDF.cint(fitted_mle, g, p=0.8)
+        @test cdf(distrib, cint[2]) - cdf(distrib, cint[1]) ≈ 0.8
+
     end
 
     @testset "returnLevelEstimation()" begin
+
+        d = 5
+        T = 50
+        @test IDF.returnLevelEstimation(fitted_mle, d, T) ≈ IDF.returnLevel(model, θ̂, d, T) 
+
     end
 
     @testset "returnLevelCint()" begin
+
+        d = 5
+        T = 50
+        g(θ) = IDF.returnLevel(model, θ, d, T) 
+        @test IDF.returnLevelCint(fitted_mle, d, T, p=0.8) ≈ IDF.cint(fitted_mle, g, p=0.8) 
+
     end
 
 end
